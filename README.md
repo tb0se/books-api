fc52343ec3240b1ff83b83efca5ec45ef1b6f444
tb0se <rachiditbose@gmail.com>
2023-10-29 14:23:50 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,15 +1,40 @@
-# Elysia with Bun runtime
+# Books Restful API
+
+A simple Restful API built while learning Elysia and Bun.
+
+## Made using
+
+- Bun
+- Elysia
+- Typescript
 
 ## Getting Started
-To get started with this template, simply paste this command into your terminal:
-```bash
-bun create elysia ./elysia-example
-```
+
+To get a local copy up and running follow these simple steps.
+
+1. Ensure you have [Bun installed](https://bun.sh/docs/installation)
+2. Install the required dependencies
+   ```bash
+   bun install
+   ```
+3. Create an `.env` file at the root of the project:
+   ```bash
+   PORT=8000
+   JWT_SECRET='supersecret
+   ```
 
 ## Development
+
 To start the development server run:
+
 ```bash
-bun run dev
+bun dev
 ```
 
-Open http://localhost:3000/ with your browser to see the result.
+Open http://localhost:8000/ with your browser to see the result.
+
+You can view the swagger documentation at http://localhost:8000/.
+
+## Credits
+
+- [Building a RESTful API with Bun and ElysiaJS](https://youtu.be/v_N97wwuvPw?si=RPEHe9bUs4lmRh4E)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,83 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from 'elysia';
+import { cookie } from '@elysiajs/cookie';
+import { jwt } from '@elysiajs/jwt';
+import { swagger } from '@elysiajs/swagger';
+import { BooksDatabase } from './db';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const PORT = Bun.env.PORT || 8000;
+
+class Unauthorized extends Error {
+	constructor() {
+		super('Unauthorized');
+	}
+}
+
+const app = new Elysia()
+	.use(swagger())
+	.error({
+		'401': Unauthorized,
+	})
+	.onError(({ code, error }) => {
+		let status;
+
+		switch (true) {
+			case code == 'VALIDATION':
+				status = 400;
+				break;
+			case code == 'NOT_FOUND':
+				status = 404;
+				break;
+			case code == '401':
+				status = 401;
+				break;
+			default:
+				status = 500;
+				break;
+		}
+
+		return new Response(error.toString(), { status: status });
+	})
+	.use(cookie())
+	.use(
+		jwt({
+			name: 'jwt',
+			secret: Bun.env.JWT_SECRET || 'supersecret',
+		})
+	)
+	.decorate('db', new BooksDatabase());
+
+app.get('/books', ({ db }) => db.getBooks());
+
+app.post('/books', ({ db, body }) => db.addBook(body), {
+	body: t.Object({
+		name: t.String(),
+		author: t.String(),
+	}),
+});
+
+app.put(
+	'/books',
+	({ db, body }) =>
+		db.updateBook(body.id, { name: body.name, author: body.author }),
+	{
+		body: t.Object({
+			id: t.Number(),
+			name: t.String(),
+			author: t.String(),
+		}),
+	}
+);
+
+app.get('/books/:id', async ({ db, params, jwt, cookie: { auth } }) => {
+	const profile = await jwt.verify(auth);
+	if (!profile) throw new Unauthorized();
+	return db.getBook(parseInt(params.id));
+});
+
+app.delete('/books/:id', ({ db, params }) => db.getBook(parseInt(params.id)));
+
+app.listen(PORT);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
